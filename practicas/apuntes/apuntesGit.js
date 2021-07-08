b37967257a71e203512c7c960d6f014523508c05
Rodriguez Lorena <loreleyro.89@gmail.com>
2021-07-08 08:06:41 -0300

    carritoStar

---
+++ practicas/apuntes/apuntesGit.js
@@ -0,0 +1,42 @@
+// // // "hola, mi nombre es" + "lorena" + "y tengo" + 31 + "años;" //añadir una variable
+
+// // // // let score = 0;
+// // // // score++;
+// // // // score; // incrementar un valor
+
+// // // // let lifePoints = 100;
+// // // // lifePoints = lifePoints - 25;
+// // // // lifePoints; 
+
+// // // // lifePoints = lifePoints + 10;
+// // // // lifePoints; // disminuir el valor de una variable
+
+// // // //mas simple
+// // // let lifePoints = 100;
+// // // lifePoints -= 25;
+// // // lifePoints;
+// // // console.log(lifePoints);
+
+// // // lifePoints += 10;
+// // // lifePoints;1
+// // // console.log(lifePoints);
+
+// // // const number = 5e10
+// // // console.log(number)
+
+// // const number = 5 % 3
+// // console.log(number)
+
+// 3 > 2
+// console.log(3 > 2)
+// 2 > 3
+// console.log(2 > 3)
+
+// typeof (3 > 2)
+// typeof (2 > 3)
+// console.log( typeof( 3> 2))
+// console.log( typeof( 2 > 3))
+
+
+const firstName = ("lorena")
+console.log("lorena")
---
+++ practicas/apuntes/proyectosGit.js
@@ -0,0 +1,28 @@
+// // // prompt("cual es tu edad?");
+
+// //  //no me aparece el input (cosnultar)
+
+
+
+
+var temperature=(prompt("cual es temperatura?"));
+var temF = (temperature* 1.8) + 32;
+document.write("tu temperatura actual es: " + temperature + " y en farenheit es: " + temF)
+
+// //segundo proyecto salio. no le inicie con la funcion antes (var), y ocupe src, de otra manera para agrgar el script al html. 
+
+
+
+// income= prompt("¿cuanto es tu ingreso?");
+// cost= prompt("¿cual es el costo?");
+// taxation= prompt("¿cual es el % de impuestos?");
+
+// grossProfit = income - cost;
+// taxationFinal = grossProfit/100;
+// finalIncome = grossProfit + taxationFinal;
+
+// document.write("$" + finalIncome);
+
+// //proyecto 3, salio. busque solamente la guia para sacar porcentajes. 
+
+
